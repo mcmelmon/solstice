b86c912c7eee32001b0969fe1383d4873e567faf
Matthew Melmon <matt.melmon@gmail.com>
2019-12-24 14:06:17 -0800

    Master commit

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform center;
+    public GameObject body;
+    public float incline = 0f;
+    public float degreesPerSecond = 1f;
+
+    private void Start() {
+        transform.Rotate(new Vector3(incline, 0, 0));
+    }
+
+    void Update()
+    {
+        transform.Rotate (0, degreesPerSecond * Time.deltaTime, 0);
+    }
+}
