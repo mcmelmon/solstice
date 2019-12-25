c596f18b9dd9548702f5e295b08096fda3c56722
Matthew Melmon <matt.melmon@gmail.com>
2019-12-24 22:02:48 -0800

    Player regalia

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,15 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 3f;
+    public float turnSpeed = 75f;
+
+    Vector2 movement;
 
     // properties
 
     public static Player Instance { get; set; }
+
+    // Unity
 
     private void Awake()
     {
@@ -30,11 +35,24 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        float rotation = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        if (!Mathf.Approximately(translation, 0) || !Mathf.Approximately(rotation, 0)) {
+            transform.Translate(0, 0, translation);
+            transform.Rotate(0, rotation, 0);
+        }
+    }
+
+
+    // public
+
+
+    public void Move() {
 
     }
 
 
-    public void Move() {
-        
+    public void Interact() {
+
     }
 }
