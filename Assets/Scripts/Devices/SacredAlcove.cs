0fe1012120e615f4bc2d4387cc99a36d50c15f19
Matthew Melmon <matt.melmon@gmail.com>
2019-12-26 20:16:53 -0800

    Turn off spotlight after buttons

--- Assets/Scripts/Devices/SacredAlcove.cs
+++ Assets/Scripts/Devices/SacredAlcove.cs
@@ -6,13 +6,38 @@ public class SacredAlcove : MonoBehaviour
 {
     public Switch mechanism;
     public GameObject sacredOrb;
+    public Light spotlight;
+
+    private void Awake() {
+        if (spotlight != null) StartCoroutine(SwingSpotlight());
+    }
 
     void Update()
     {
         if (sacredOrb != null && mechanism != null && mechanism.Triggered) {
-            Debug.Log("triggered");
             sacredOrb.GetComponent<Rigidbody>().useGravity = true;
             sacredOrb = null;
+            StopCoroutine(SwingSpotlight());
+            spotlight.enabled = false;
         }
     }
+
+    // private
+
+    IEnumerator SwingSpotlight()
+    {
+        Vector3 originalRotation = spotlight.transform.rotation.eulerAngles;
+        int accumulator = 0;
+
+        while(true) {
+            accumulator += 1;
+            if (accumulator > 360) accumulator = 0;
+            float radians = accumulator * Mathf.PI / 180f;
+            float adjustment = Mathf.Sin(radians) * 90f;
+
+            spotlight.transform.eulerAngles = new Vector3(originalRotation.x, originalRotation.y + adjustment, originalRotation.z);
+            
+            yield return new WaitForSeconds(.05f);
+        }
+    }    
 }
