cda9cc44fcc320481d057e62e957b5420b01a006
Matthew Melmon <matt.melmon@gmail.com>
2020-01-02 16:02:19 -0800

    Platform objectives and code cleanup

---
+++ Assets/Scripts/Devices/Orb.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+    public int secondsBeforeRise;
+    public GameObject celestialCycle;
+
+    public Light Lamp { get; set; }
+    Vector3 RespawnPoint { get; set; }
+
+    private void Awake() {
+        Lamp = GetComponentInChildren<Light>();
+        RespawnPoint = transform.position;
+    }
+    public void AscendToHeaven() 
+    {
+        StartCoroutine(Ascend());
+    }
+    public IEnumerator Ascend()
+    {
+        while (true) {
+            yield return new WaitForSeconds(secondsBeforeRise);
+            celestialCycle.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+    
+    public void Dim() {
+        Lamp.enabled = false;
+    }
+
+    public void Illuminate() {
+        Lamp.enabled = true;
+    }
+
+    public void LockInPlace() {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void Respawn() {
+        transform.position = RespawnPoint;
+    }
+
+    public void UnlockInPlace() {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    }
+}
---
+++ Assets/Scripts/Devices/Reactor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reactor : MonoBehaviour
+{
+    public Socket Socket { get; set; }
+
+    Switch Mechanism { get; set; }
+
+    private void Awake() {
+        Mechanism = GetComponentInChildren<Switch>();
+        Socket = GetComponentInChildren<Socket>();
+    }
+
+    void Update()
+    {
+        ShowButton();
+
+        if (Socket != null && Socket.Orb != null && Mechanism != null && Mechanism.Engaged) {
+            Socket.Orb.Illuminate();
+            Socket.Orb.UnlockInPlace();
+        }
+    }
+
+    // private
+
+    void ShowButton() {
+        if (Socket == null || Socket.Orb == null || Mechanism == null || Mechanism.Engaged) {
+            Mechanism.buttons[0].gameObject.SetActive(false);
+        } else if (Socket.Orb != null) {
+            Mechanism.buttons[0].gameObject.SetActive(true);
+            if (!Mechanism.Engaged) Socket.Orb.LockInPlace();
+        }
+    }
+}
